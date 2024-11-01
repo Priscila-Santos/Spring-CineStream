c40f4286ecf47289b3f173bc58068ec8f1cf360a
Lucas Alecsander <lucasalecsander10@hotmail.com>
2024-11-01 19:39:18 -0300

    Projeto FullStack -  CineStream

---
+++ backend/src/main/java/com/tech/ada/spring_cinestream/SpringCinestreamApplication.java
@@ -0,0 +1,13 @@
+package com.tech.ada.spring_cinestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCinestreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCinestreamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tech/ada/spring_cinestream/SpringCinestreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.ada.spring_cinestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCinestreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
